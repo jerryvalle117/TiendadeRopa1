27fb40a8248187c8df7b7356f44ff35e37d34c1e
marce <marce@DESKTOP-76F7GM8>
2021-04-10 23:49:36 -0600

    actualizacion x2

--- proyectoTiendadeRopa/proyectoTiendadeRopa.Web/Controllers/HomeController.cs
+++ proyectoTiendadeRopa/proyectoTiendadeRopa.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendadeRopa.BL;
 
 namespace proyectoTiendadeRopa.Web.Controllers
 {
@@ -11,7 +13,13 @@ namespace proyectoTiendadeRopa.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl =
+                ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+            return View(listadeProductos);
         }
     }
 }
